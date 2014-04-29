e0c2f67c9237a9137bfab5961d0ae58a1aafe309
Lucian Barticel <lucian.barticel@comodo.com>
2014-04-29 09:43:22 +0300

    added a nice login system

--- nodev2/routes/index.js
+++ nodev2/routes/index.js
@@ -1,10 +1,40 @@
-exports.init = function (app){
-    app.get('/', function (req, res){
+exports.init = function(app) {
+
+    function checkAuth(req, res, next) {
+        if (!req.session.username) {
+            res.redirect('/login');
+        } else {
+            next();
+        }
+    }
+
+    app.get('/login', function(req, res) {
+        res.header("Content-Type", "text/html; charset=utf-8");
+        res.render('login', {title: "please log in", content:"Insert credentials bellow"});
+    });
+
+    app.post('/login', function(req, res) {
+        var post = req.body;
+        if (post.userName === 'lucian' && post.passWord === 'lucianpass') {
+            req.session.username = 'lucian';
+            res.redirect('/');
+        } else {
+            res.render('login', {title: "please log in", content:"Bad username or password. Try again"});
+        }
+    });
+
+    app.get('/', checkAuth, function(req, res) {
         res.header("Content-Type", "text/html; charset=utf-8");
         res.render('home', {title: "appLuc", name: "Lucian"});
     });
-    app.post('/', function(req, res){
+    app.post('/', function(req, res) {
 //        console.log(req.body);
-        res.render('home', {title: "appLuc", name: req.body.someName});
-    })
+        res.render('home', {title: "appLuc", name: req.body.someName, content: req.body.someContent});
+    });
+
+    app.get('/logout', function(req, res) {
+        delete req.session.username;
+        res.redirect('/login');
+    });
+
 };
--- nodev2/server.js
+++ nodev2/server.js
@@ -1,10 +1,16 @@
 var express = require("express");
 var jade = require("jade");
 var bodyParser = require("body-parser");
+var session = require("cookie-session");
 var routes = require("./routes");
+//var less = require("less");
 var app = express();
 
+//less.render('.greeting { color: green }', function (e, css) {
+//  console.log(css);
+//});
 
+app.use(session({secret: '1234567890QWERTY'}));
 app.set("views", __dirname+"/views");
 app.set("view engine", "jade");
 app.use(bodyParser());
